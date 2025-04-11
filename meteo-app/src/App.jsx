a1b6490b49719887d89720f4b2441f4e53cb2698
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-11 11:34:05 +0200

    Update

---
+++ meteo-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import MeteoCard from "./components/MeteoCard";
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+        <MeteoCard />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ meteo-app/src/components/MeteoCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/meteoCard.css";
+
+const MeteoCard = () => {
+  return (
+    <>
+      <h1>Meteo</h1>
+    </>
+  );
+};
+
+export default MeteoCard;
