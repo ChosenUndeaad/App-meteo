5c0dcb8e8fd6c36a8db940a752e40588e5c3d2ba
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-11 11:56:48 +0200

    Updated card body

--- meteo-app/src/components/MeteoCard.jsx
+++ meteo-app/src/components/MeteoCard.jsx
@@ -3,9 +3,27 @@ import "../style/meteoCard.css";
 
 const MeteoCard = () => {
   return (
-    <>
-      <h1>Meteo</h1>
-    </>
+    <div className='meteo-card'>
+      <h1>Previsioni</h1>
+      <div className='search-bar'>
+        <input type='text' placeholder='Cerca una città ' />
+        <button type='button'>Cerca</button>
+      </div>
+
+      <img
+        src='https://www.mrw.it/wp-content/uploads/2020/12/0iwkf4_1609360688.jpg'
+        alt='Icona del tempo'
+        className='tempo-icona'
+      />
+      <p className='temperatura'>16 C</p>
+      <p className='luogo'>Città</p>
+      <div className='meteo-data'>
+        <div className='col'>
+          <p>Umidità</p>
+          <span>UMI</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
